76f222eb0b97eb7ba5f518138619ed06274afe4d
vannchhai123 <vannchhai.sti@gmail.com>
2026-04-21 16:25:19 +0700

    update cors

--- src/main/java/com/norton/backend/config/SecurityConfig.java
+++ src/main/java/com/norton/backend/config/SecurityConfig.java
@@ -122,7 +122,8 @@ public class SecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
 
-    configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+    configuration.setAllowedOrigins(
+        List.of("http://localhost:3000", "https://general-affair-app.vercel.app"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
