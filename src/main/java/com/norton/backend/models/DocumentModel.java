2f5f613c86e4e4d19ae4c9070fe0c2f39dd0d761
vannchhai123 <vannchhai.sti@gmail.com>
2026-07-29 18:22:42 +0700

    fixed

--- src/main/java/com/norton/backend/models/DocumentModel.java
+++ src/main/java/com/norton/backend/models/DocumentModel.java
@@ -44,7 +44,6 @@ import lombok.*;
       @Index(name = "idx_docs_status_date", columnList = "status, document_date")
     })
 public class DocumentModel extends BaseIdModel {
-
   @Column(nullable = false, unique = true)
   private UUID uuid;
 
