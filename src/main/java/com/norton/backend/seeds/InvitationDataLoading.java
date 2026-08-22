981c0dbcc4923828718d186204455b97397a4ec9
vannchhai123 <vannchhai.sti@gmail.com>
2026-08-22 17:12:28 +0700

    update ui

--- src/main/java/com/norton/backend/seeds/InvitationDataLoading.java
+++ src/main/java/com/norton/backend/seeds/InvitationDataLoading.java
@@ -13,6 +13,7 @@ import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -20,7 +21,7 @@ import org.springframework.stereotype.Component;
 @DependsOn("officerDataLoading")
 @RequiredArgsConstructor
 @Order(9)
-// @Profile("dev")
+@Profile("dev")
 public class InvitationDataLoading implements CommandLineRunner {
 
   private final InvitationRepository invitationRepository;
